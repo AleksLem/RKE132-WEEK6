fea924d48634d48842003128ebdb608e9d07f900
Aleks Lemming <aleks.lemming@tktk.ee>
2023-03-09 18:47:36 +0200

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "suschi", "pizza", "burger", "chicken wings", "takeaway" };
+
+Random rnd= new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we re gona eat {snacks[randomIndex]}.");
---
+++ Week6StringAndArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+string hello = " hello, world! Here I am! ";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello= hello.Trim();
+//int wordCounter = 1;
+
+char firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter= hello[0];
+
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+
+//Console.WriteLine(trimmedString.Length);
+
+//for (int i=0; i < hello.Length; i++)
+
+    
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+
+
+//    //Console.WriteLine($"{i} symbol is string: {hello[i]}");
+//    //if (hello[i] == ' ')
+//   // { 
+//        //wordCounter++;
+//          //  }
+
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter + 1} words ");
